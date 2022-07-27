b94329e071e1b13392c6b196b50eb91f632aaa99
wuxue <wudaxue123@gmail.com>
2022-07-27 16:45:14 +0800

    import element-plus

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
 
 <template>
   <div class="flex justify-center"  style="width: 100vw">
-    111
+    <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span>Card name</span>
+        <el-button class="button" text>Operation button</el-button>
+      </div>
+    </template>
+    <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+  </el-card>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,46 @@ import vue from '@vitejs/plugin-vue'
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      dts: true,
+      dirs: ['src/components'],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass',
+        }),
+      ],
+    }),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+        /\.json$/, // .json
+      ],
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+      ],
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+      resolvers: [() => null],
+      dts: './auto-imports.d.ts',
+    }),
+  ],
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],

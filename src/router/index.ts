9a11b21e2ed67b63fde6bd0aaba81f47baa48dc5
wuxue <wudaxue123@gmail.com>
2022-08-12 00:51:42 +0800

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,48 +7,13 @@ const routes = [
     component: Layout,
     redirect: '/index',
     name: 'base',
-    meta: { title: '基础信息管理' },
+    meta: { title: 'home' },
     children: [
       {
-        component: () => import('@/views/company/index.vue'),
+        component: () => import('@/views/home/index.vue'),
         path: 'index',
-        name: 'company',
-        meta: { title: '企业基本信息' },
-      },
-      {
-        component: () => import('@/views/passenger/index.vue'),
-        path: 'passenger',
-        name: 'passenger',
-        meta: { title: '乘车人员管理' },
-      },
-    ],
-  },
-  {
-    path: '/trip',
-    component: Layout,
-    name: 'trip',
-    meta: { title: '车次管理' },
-    children: [
-      {
-        component: () => import('@/views/plan_trip/index.vue'),
-        path: 'index',
-        name: 'trip',
-        meta: { title: '车次管理' },
-      },
-    ],
-  },
-  {
-    path: '/order',
-    component: Layout,
-    redirect: '/order',
-    name: 'order',
-    meta: { title: '订单管理' },
-    children: [
-      {
-        component: () => import('@/views/order/index.vue'),
-        path: 'index',
-        name: 'order',
-        meta: { title: '订单管理' },
+        name: 'home',
+        meta: { title: 'home' },
       },
     ],
   },
--- src/utils/global_main_loader.ts
+++ src/utils/global_main_loader.ts
@@ -1,15 +1,10 @@
 import 'virtual:svg-icons-register'
 import Adaptive from '@/directive/el-table/adaptive'
-import BaiduMapVue3 from 'baidu-map-vue3'
-import { baiduAK } from '@/const'
 import { App } from 'vue'
 
 export default function performLoader(app: App<Element>) {
   // thirdparty component
   app.directive('adaptive', Adaptive)
-  app.use(BaiduMapVue3, {
-    apiKey: baiduAK,
-  })
   // mixin
   app.mixin({
     methods: {
